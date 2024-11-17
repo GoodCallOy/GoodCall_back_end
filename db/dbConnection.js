182ffea9c16faad4fffc1d48f573859c16c0f84a
jasond <jasondansie@gmail.com>
2024-11-17 17:08:48 +0200

    adds the back end

---
+++ db/dbConnection.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+require('dotenv').config();
+const options = {
+    authSource: 'admin',
+    user: `${process.env.MONGO_USER}`,
+    pass: `${process.env.MONGO_PASS}`,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+const connectDB = async () => {
+    try {
+        await mongoose_1.default.connect(`${process.env.MONGO_URI}`, options);
+        console.log('Connected to MongoDB');
+    }
+    catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+    }
+};
+exports.default = connectDB;
---
+++ models/agent.ts
@@ -0,0 +1,56 @@
+import mongoose, { Document, Model, Schema  } from 'mongoose';
+
+export interface IAgent extends Document {
+    name: string;
+    meetings: number;
+    call_time: number;
+    calls_made: number;
+    outgoing_calls: number;
+    answered_calls: number;
+    response_rate: number;
+    case: string;
+    create_date: Date;
+}
+
+const agentSchema: Schema<IAgent> = new Schema<IAgent>({
+    name: {
+    type: String,
+    required: true,
+  },
+  meetings: {
+    type: Number,
+    required: false,
+  },
+  call_time: {
+    type: Number,
+    required: true,
+  },
+  calls_made: {
+    type: Number,
+    required: true,
+  },
+  outgoing_calls: {
+    type: Number,
+    required: true,
+  },
+  answered_calls: {
+    type: Number,
+    required: false,
+  },
+  response_rate: {
+    type: Number,
+    required: false,
+  },
+  case: {
+    type: String,
+    required: false,
+  },
+  create_date: {
+    type: Date,
+    required: false,
+  },
+});
+
+const Agent: Model<IAgent> = mongoose.model<IAgent>('Job', agentSchema);
+
+export default Agent;
