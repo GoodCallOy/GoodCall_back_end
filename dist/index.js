94975c9488125edc646ecf9b3612d77c26926289
jasond <jasondansie@gmail.com>
2025-01-11 20:50:01 +0200

    fixes render.com build errors

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,11 @@ const dbConnection_1 = __importDefault(require("./db/dbConnection"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-const agentStats_1 = __importDefault(require("./routes/agentStats"));
 const cases_1 = __importDefault(require("./routes/cases"));
+const agentStats_1 = __importDefault(require("./routes/agentStats"));
+const agent_1 = __importDefault(require("./routes/agent"));
 app.use('/api/v1/agentStats', agentStats_1.default);
+app.use('/api/v1/agent', agent_1.default);
 app.use('/api/v1/cases', cases_1.default);
 app.listen(serverConfig_json_1.port, () => {
   console.log(`Server is listening on port ${serverConfig_json_1.port}...`);
