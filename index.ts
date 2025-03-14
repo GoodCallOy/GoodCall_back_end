3e584a8421e80eb026f37bd135edb1aa4f8e134d
Jason Dansie <jasondansie@gmail.com>
2025-03-14 09:16:39 +0200

    adds monthkey to addAgentGoals

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
-
 import { port, host } from './serverConfig.json';
-
-
 import connectDB from './src/db/dbConnection';
 
+// Connect to the database
 connectDB();
 
 const app = express();
@@ -14,17 +12,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Prevent caching responses
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+// Import routers
 import caseRouter from './src/routes/cases';
 import agentStatsRouter from './src/routes/agentStats';
 import agentRouter from './src/routes/agent';
 import agentGoalsRouter from './src/routes/agentGoals';
 
+// Define API routes
 app.use('/api/v1/agentstats', agentStatsRouter);
 app.use('/api/v1/agent', agentRouter);
 app.use('/api/v1/cases', caseRouter);
 app.use('/api/v1/agentgoals', agentGoalsRouter);
 
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
 });
