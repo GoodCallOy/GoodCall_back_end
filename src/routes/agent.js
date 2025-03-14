044fcd08d2421a5bd51e74255015fd1a7d2bc6a5
Jason Dansie <jasondansie@gmail.com>
2025-03-14 15:22:41 +0200

    fixes routing for getAgentByName

--- src/routes/agent.js
+++ src/routes/agent.js
@@ -5,5 +5,5 @@ const agent_1 = require("../controllers/agent");
 const router = (0, express_1.Router)();
 router.route("/").get(agent_1.getAllAgents).post(agent_1.addAgent);
 router.route("/:id").get(agent_1.getAgentById).post(agent_1.modifyAgent).delete(agent_1.deleteAgent);
-router.get('/:name', agent_1.getAgentByName);
+router.route('/getagent/:name').get(agent_1.getAgentByName);
 exports.default = router;
