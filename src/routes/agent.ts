3b1f8c711e647178654993f2921dc8a7efa4a360
Jason Dansie <jasondansie@gmail.com>
2025-03-14 14:56:25 +0200

    added find agent by name

--- src/routes/agent.ts
+++ src/routes/agent.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAllAgents, addAgent, getAgentById, modifyAgent, deleteAgent } from "../controllers/agent";
+import { getAllAgents, addAgent, getAgentById, modifyAgent, deleteAgent, getAgentByName } from "../controllers/agent";
 
 const router = Router();
 
 router.route("/").get(getAllAgents).post(addAgent);
 
 router.route("/:id").get(getAgentById).post(modifyAgent).delete(deleteAgent);
+
+router.get('/:name', getAgentByName);
 
 export default router as Router;
