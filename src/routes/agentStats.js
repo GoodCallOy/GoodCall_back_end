9604cba215f5cc999ed73cfc9482924d49cca0c3
jasond <jasondansie@gmail.com>
2025-10-05 10:46:27 +0300

    changes how the build is configured

--- src/routes/agentStats.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const agentStats_1 = require("../controllers/agentStats");
-const router = (0, express_1.Router)();
-router.route("/").get(agentStats_1.getAllAgentStats).post(agentStats_1.addAgentStats);
-router.route("/:id").get(agentStats_1.getAgentStatsById).post(agentStats_1.modifyAgentStats).delete(agentStats_1.deleteAgentStats);
-exports.default = router;
